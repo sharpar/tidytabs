666f76c3a8c4f20f89ce2386979918dfaba299c0
Bill Feng <bill.zijing.feng@gmail.com>
2016-03-23 17:23:55 -0400

    experimenting

--- TidyTabs/index.js
+++ TidyTabs/index.js
@@ -1,13 +1,56 @@
 var self = require("sdk/self");
 var buttons = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
 
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
 function dummy(text, callback) {
   callback(text);
-  
+}
+
+var tabs = require("sdk/tabs");
+tabs.on("open", checkEmpty);
+tabs.on("ready", checkDuplicate);
+
+/* Feature 1
+ * Closes other blank pages when opening a new tab.
+ * If already focused on a blank page, a new tab will not be opened.
+ */
+function checkEmpty(newTab) {
+  if((tabs.activeTab.url == "about:blank") || (tabs.activeTab.url == "about:newtab")){
+    newTab.close();
+  }else{
+    for (let tab of tabs){
+      if((tab != newTab) && ((tab.url == "about:blank") || (tab.url == "about:newtab"))){
+        tab.close();
+      }
+    }
+  }
+}
+
+function checkDuplicate(newTab) {
+  var tabs = require("sdk/tabs");
+    for (let tab of tabs){
+      if((tab != newTab) && (tab.url == newTab.url)){
+        console.log("old tab: " + tab.url);
+        console.log("new tab: " + newTab.url);
+        tab.activate();
+        newTab.close();
+      }
+    }
+}
+
+function checkLink(link){
+  var tabs = require("sdk/tabs");
+    for (let tab of tabs){
+      if((tab != newTab) && (tab.url == link)){
+        console.log("old tab: " + tab.url);
+        console.log("new tab: " + link);
+        tab.activate();
+        return true;
+      }
+    }
+    return false;
 }
 
 //Example From https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/List_Open_Tabs
@@ -18,54 +61,33 @@ require("sdk/ui/button/action").ActionButton({
 	  onClick: listTabs
 	});
 
-	function listTabs() {
-	  var tabs = require("sdk/tabs");
-	  for (let tab of tabs)
-	    console.log(tab.url);
-	}
-
-require("sdk/tabs").on("ready", checkDuplicate);
- 
-function checkDuplicate(newTab) {
-	var tabs = require("sdk/tabs");
-  	for (let tab of tabs){
-  		if((tab != newTab) && (tab.url == newTab.url)){
-  			console.log("old tab: " + tab.url);
-  			console.log("new tab: " + newTab.url);
-  			tab.activate();
-  			newTab.close();
-  		}
-  	}
+function listTabs() {
+  var tabs = require("sdk/tabs");
+  for (let tab of tabs)
+    console.log(tab.url);
 }
 
-function checkLink(link){
-	var tabs = require("sdk/tabs");
-  	for (let tab of tabs){
-  		if((tab != newTab) && (tab.url == link)){
-  			console.log("old tab: " + tab.url);
-  			console.log("new tab: " + link);
-  			tab.activate();
-  			return true;
-  		}
-  	}
-  	return false;
+// This is for the pop up menu in the browser
+/*
+var {Cu, Ci} = require('chrome');
+Cu.import('resource://gre/modules/Services.jsm');
+var doit = Services.prompt.confirm(null, 'title', 'message');
+if (doit) { 
+//he clicked yes
 }
 
-/* Feature 1
- * Closes other blank pages when opening a new tab.
- * If already focused on a blank page, a new tab will not be opened.
- */
-var tabs = require("sdk/tabs");
-tabs.on('open', function onOpen(newTab) {
-  if((tabs.activeTab.url == "about:blank") || (tabs.activeTab.url == "about:newtab")){
-    newTab.close();
-  }else{
-    for (let tab of tabs){
-      if((tab != newTab) && ((tab.url == "about:blank") || (tab.url == "about:newtab"))){
-        tab.close();
-      }
-    }
+// This is for a notification on your computer
+/*
+var notifications = require("sdk/notifications");
+notifications.notify({
+  title: "Jabberwocky",
+  text: "'Twas brillig, and the slithy toves",
+  data: "did gyre and gimble in the wabe",
+  onClick: function (data) {
+    console.log(data);
+    // console.log(this.data) would produce the same result.
   }
 });
+*/
 
 exports.dummy = dummy;
