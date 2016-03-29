d246cfb108249b484cdd10381160fc59f7379017
MsClaudz <claudzchen@gmail.com>
2016-03-29 15:06:26 -0400

    added sample panel code that brings up a panel from a new button

--- TidyTabs/index.js
+++ TidyTabs/index.js
@@ -128,6 +128,41 @@ function listTabs() {
         console.log(tab.url);
 }
 
+//default sample panel code from panel ref page
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
+
+var button = ToggleButton({
+  id: "my-button",
+  label: "my button",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onChange: handleChange
+});
+
+var panel = panels.Panel({
+  width: 180,
+  height: 180,
+  contentURL: self.data.url("notification.html"),
+  onHide: handleHide
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      position: button
+    });
+  }
+}
+
+function handleHide() {
+  button.state('window', {checked: false});
+}
+
 // This is for the pop up menu in the browser
 /*
 var {Cu, Ci} = require('chrome');
